7538f87cfecf30b24d9b5b305182058ed5e6d735
diffth <diffth@nate.com>
2024-09-24 14:56:13 +0900

    0924 2st commit~ SReplyController.java modify

--- src/main/java/com/healthplan/work/Controller/SReplyController.java
+++ src/main/java/com/healthplan/work/Controller/SReplyController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sreplies")
 public class SReplyController {
 
-//    @Autowired
-//    private SReplyService sreplyService;
+    @Autowired
+    private SReplyService sreplyService;
 
 }
