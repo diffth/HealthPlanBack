909806b4e82baa2ca3a2d725f0f80a9f98344a1b
diffth <diffth@nate.com>
2024-10-04 17:58:33 +0900

    1004 6st commit~ MemberController.java 회원관리

--- src/main/java/com/healthplan/work/Controller/MemberController.java
+++ src/main/java/com/healthplan/work/Controller/MemberController.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,35 +29,53 @@ import java.util.List;
 import java.util.Map;
 
 
+/**
+ * The type Member controller.
+ */
 @RestController
+@Log4j2
 @RequestMapping("/member")
 public class MemberController {
 
     private final PasswordEncoder passwordEncoder;
+    /**
+     * The Mapper.
+     */
     @Autowired
     MemberMapper mapper;
 
     @Autowired
     private JwtUtils jwtUtils = new JwtUtils();
 
+    /**
+     * Instantiates a new Member controller.
+     *
+     * @param passwordEncoder the password encoder
+     */
     @Autowired
     // private PasswordEncoder passwordEncoder;
 
-    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
-     public MemberController(PasswordEncoder passwordEncoder) {
+    public MemberController(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // 회원리스트
+    /**
+     * Select list list.
+     *
+     * @param model the model
+     * @return the list
+     * @throws Exception the exception
+     */
+// 회원리스트
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<MemberEntity> selectList(Model model) throws Exception {
 
-        logger.info("//****************************** /member/list");
+        log.info("//****************************** /member/list");
 
         List<MemberEntity> list = mapper.selectMemberList();
 
-        logger.info("// list.toString()=" + list.toString());
+        log.info("// list.toString()=" + list.toString());
 
         return list;
     }
@@ -72,13 +92,20 @@ public class MemberController {
         return num;
     }*/
 
+    /**
+     * Gets uuid by mno.
+     *
+     * @param mem the mem
+     * @return the uuid by mno
+     * @throws Exception the exception
+     */
     @RequestMapping(value = "/getUuidByMno", method = RequestMethod.POST)
     public ResponseEntity<Map<String, String>> getUuidByMno(@RequestBody MemberEntity mem) throws Exception {
-        logger.info("회원정보 조회 요청 - mno: " + mem.getMno());
+        log.info("회원정보 조회 요청 - mno: " + mem.getMno());
 
         // mno로 uuid 조회
         String uuid = mapper.selectUuidByMno(mem.getMno());
-        logger.info("/******************************* uuid 조회 당한 회원번호 보여줘 : " + uuid);
+        log.info("/******************************* uuid 조회 당한 회원번호 보여줘 : " + uuid);
 
         if (uuid != null) {
             // 응답 데이터를 JSON 형식으로 반환
@@ -93,27 +120,41 @@ public class MemberController {
     }
 
 
+    /**
+     * Gets uuids by mnos.
+     *
+     * @param mnos the mnos
+     * @return the uuids by mnos
+     * @throws Exception the exception
+     */
     @PostMapping("/getUuidsByMnos")
     public Map<Integer, String> getUuidsByMnos(@RequestBody List<Integer> mnos) throws Exception {
         return mapper.getUuidsByMnos(mnos);
     }
 
 
-    // 회원번호 조회
+    /**
+     * Select mno response entity.
+     *
+     * @param requestData the request data
+     * @return the response entity
+     * @throws Exception the exception
+     */
+// 회원번호 조회
     @RequestMapping(value = "/readMno", method = RequestMethod.POST)
     public ResponseEntity<MemberEntity> selectMno(@RequestBody Map<String, String> requestData) throws Exception {
 
         // Map에서 "uuid" 값 추출
         String uuid = requestData.get("uuid");
         // 로그로 uuid 확인
-        logger.info("조회할 아이디 : " + uuid);
+        log.info("조회할 아이디 : " + uuid);
 
         MemberEntity memberInfo = mapper.selectMno(uuid);
 
-        logger.info("/************************** 일단 회원번호 정보 보여줘" + memberInfo);
+        log.info("/************************** 일단 회원번호 정보 보여줘" + memberInfo);
 
         if (memberInfo != null) {
-            logger.info("/************************** 성공한 회원번호 정보 보여줘" + memberInfo);
+            log.info("/************************** 성공한 회원번호 정보 보여줘" + memberInfo);
             return ResponseEntity.ok(memberInfo);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -121,13 +162,19 @@ public class MemberController {
     }
 
 
-    /// 회원가입
-
+    /**
+     * Insert mem post response entity.
+     *
+     * @param mem the mem
+     * @return the response entity
+     * @throws Exception the exception
+     */
+/// 회원가입
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<String> insertMemPOST(@RequestBody MemberEntity mem) throws Exception {
 
-        logger.info("/*********************** 회원가입!! regist post ...........");
-        logger.info(mem.toString());
+        log.info("/*********************** 회원가입!! regist post ...........");
+        log.info(mem.toString());
 
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(mem.getUpw());
@@ -146,29 +193,36 @@ public class MemberController {
     }
 
 
-    // 로그인
+    /**
+     * Login post response entity.
+     *
+     * @param dto the dto
+     * @return the response entity
+     * @throws Exception the exception
+     */
+// 로그인
     @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
-    public  ResponseEntity<?> loginPOST(@RequestBody LoginDTO dto) throws Exception {
+    public  ResponseEntity<?> loginPOST(@RequestBody @NotNull LoginDTO dto) throws Exception {
 
-        logger.info("/******************************************** loginPost");
-        logger.info(dto.toString());
+        log.info("/******************************************** loginPost");
+        log.info(dto.toString());
 
         try {
             final String token = jwtUtils.generateToken(dto.getUuid());
-            logger.info("/*** 인코딩 !!!! token=" + token);
+            log.info("/*** 인코딩 !!!! token=" + token);
 
             // JWT 토큰 유효성 검사
             boolean isValidToken = jwtUtils.validateToken(token, dto.getUuid());
-            logger.info("토큰 유효성: " + isValidToken);
+            log.info("토큰 유효성: " + isValidToken);
 
             // 저장된 해시된 비밀번호 가져오기
             String storedHashedPassword = mapper.getHashedPasswordByUuid(dto.getUuid());
-            logger.info("저장된 해시된 비밀번호 가져오기!!!! Stored Hashed Password: " + storedHashedPassword);
+            log.info("저장된 해시된 비밀번호 가져오기!!!! Stored Hashed Password: " + storedHashedPassword);
 
             // 비밀번호 비교
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             boolean isPasswordMatch = passwordEncoder.matches(dto.getUpw(), storedHashedPassword);
-            logger.info("비밀번호 일치 여부: " + isPasswordMatch);
+            log.info("비밀번호 일치 여부: " + isPasswordMatch);
 
             if (isPasswordMatch) {
                 // 로그인 성공 시, 토큰과 사용자 정보를 응답 본문과 헤더에 포함하여 클라이언트에 전달
@@ -190,16 +244,23 @@ public class MemberController {
                 return new ResponseEntity<>("비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED);
             }
         } catch (Exception e) {
-            logger.error("로그인 처리 중 오류 발생", e);
+            log.error("로그인 처리 중 오류 발생", e);
             return new ResponseEntity<>("서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    // 쿠키 관리
+    /**
+     * Login cookie response entity.
+     *
+     * @param requestData the request data
+     * @return the response entity
+     * @throws Exception the exception
+     */
+// 쿠키 관리
     @RequestMapping(value = "/loginCookie", method = RequestMethod.POST)
-    public  ResponseEntity<?> loginCookie(@RequestBody Map<String, String> requestData) throws Exception {
+    public  ResponseEntity<?> loginCookie(@RequestBody @NotNull Map<String, String> requestData) throws Exception {
 
-        logger.info("/*************** /loginCookie 시작...");
+        log.info("/*************** /loginCookie 시작...");
         //logger.info("/*************** dto 뭐 받았니"+dto.toString());
 
         String token = requestData.get("token");  // token 값 추출
@@ -215,7 +276,7 @@ public class MemberController {
 
             // 토큰이 유효한지 확인
             if (jwtUtils.validateToken(token, uuid)) {
-                logger.info("/*************** token의 id 보여줘  " + uuid);
+                log.info("/*************** token의 id 보여줘  " + uuid);
 
                 return ResponseEntity.ok(Map.of("uuid", uuid));  // 유효하면 uuid를 반환
 
@@ -229,7 +290,16 @@ public class MemberController {
 
     }
 
-    // 로그아웃
+    /**
+     * Logout string.
+     *
+     * @param request  the request
+     * @param response the response
+     * @param session  the session
+     * @return the string
+     * @throws Exception the exception
+     */
+// 로그아웃
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session)
             throws Exception {
@@ -254,55 +324,76 @@ public class MemberController {
         return "success";
     }
 
-    //아이디 중복체크
+    /**
+     * Uuid ck int.
+     *
+     * @param requestData the request data
+     * @return the int
+     * @throws Exception the exception
+     */
+//아이디 중복체크
     @PostMapping("/uuidCk")
     public int uuidCk(@RequestBody Map<String, String> requestData) throws Exception {
 
-        logger.info("1. /******************** 포스트 돌겠습니다 !!! uuidCk post ...........");
+        log.info("1. /******************** 포스트 돌겠습니다 !!! uuidCk post ...........");
 
         // Map에서 "uuid" 값 추출
         String uuid = requestData.get("uuid");
 
         // 로그로 uuid 확인
-        logger.info("2. 조회할 아이디 : " + uuid);
+        log.info("2. 조회할 아이디 : " + uuid);
 
         // logger.info("/******************** 포스트 돌겠습니다 !!! uuidCk post ..........."+ uuid);
-        logger.info("3. "+uuid.toString());
+        log.info("3. "+uuid.toString());
 
         int result = mapper.uuidCk(uuid);
 
-        logger.info("4. result 값 확인: "+result);
+        log.info("4. result 값 확인: "+result);
 
         return result;
 
     }
 
-    // 이메일 중복체크
+    /**
+     * Email ck int.
+     *
+     * @param email the email
+     * @return the int
+     * @throws Exception the exception
+     */
+// 이메일 중복체크
     @RequestMapping(value = "/emailCk", method = RequestMethod.POST)
     public int emailCk(@RequestBody String email) throws Exception {
-        logger.info("/******************** 포스트 돌겠습니다 !! emailCk post ...........");
-        logger.info(email.toString());
+        log.info("/******************** 포스트 돌겠습니다 !! emailCk post ...........");
+        log.info(email.toString());
 
         int result = mapper.emailCk(email);
 
         return result;
     }
 
-    // 이름 조회
+    /**
+     * Select name response entity.
+     *
+     * @param requestData the request data
+     * @return the response entity
+     * @throws Exception the exception
+     */
+// 이름 조회
     @RequestMapping(value = "/readName", method = RequestMethod.POST)
     public ResponseEntity<MemberEntity> selectName(@RequestBody Map<String, String> requestData) throws Exception {
 
         // Map에서 "uuid" 값 추출
         String uuid = requestData.get("uuid");
         // 로그로 uuid 확인
-        logger.info("조회할 아이디 : " + uuid);
+        log.info("조회할 아이디 : " + uuid);
 
         MemberEntity memberInfo = mapper.selectName(uuid);
 
-        logger.info("/************************** 일단 이름 정보 보여줘" + memberInfo);
+        log.info("/************************** 일단 이름 정보 보여줘" + memberInfo);
 
         if (memberInfo != null) {
-            logger.info("/************************** 성공한 이름 정보 보여줘" + memberInfo);
+            log.info("/************************** 성공한 이름 정보 보여줘" + memberInfo);
             return ResponseEntity.ok(memberInfo);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -310,21 +401,28 @@ public class MemberController {
     }
 
 
-    // 마이페이지 회원정보 조회
+    /**
+     * Select mem id response entity.
+     *
+     * @param requestData the request data
+     * @return the response entity
+     * @throws Exception the exception
+     */
+// 마이페이지 회원정보 조회
     @RequestMapping(value = "/read", method = RequestMethod.POST)
     public ResponseEntity<MemberEntity> selectMemId(@RequestBody Map<String, String> requestData) throws Exception {
 
         // Map에서 "uuid" 값 추출
         String uuid = requestData.get("uuid");
         // 로그로 uuid 확인
-        logger.info("조회할 아이디 : " + uuid);
+        log.info("조회할 아이디 : " + uuid);
 
         MemberEntity memberInfo = mapper.selectUuid(uuid);
 
-        logger.info("/************************** 일단 마이페이지 정보 보여줘" + memberInfo);
+        log.info("/************************** 일단 마이페이지 정보 보여줘" + memberInfo);
 
         if (memberInfo != null) {
-            logger.info("/************************** 성공한 마이페이지 정보 보여줘" + memberInfo);
+            log.info("/************************** 성공한 마이페이지 정보 보여줘" + memberInfo);
             return ResponseEntity.ok(memberInfo);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -332,11 +430,19 @@ public class MemberController {
         // return mapper.selectUuid(mem.getUuid());
     }
 
-    // 마이페이지 회원정보 수정
+    /**
+     * Update post string.
+     *
+     * @param mem  the mem
+     * @param rttr the rttr
+     * @return the string
+     * @throws Exception the exception
+     */
+// 마이페이지 회원정보 수정
     @RequestMapping(value = "/modify", method = RequestMethod.POST)
     public String updatePOST(@RequestBody MemberEntity mem, RedirectAttributes rttr) throws Exception {
 
-        logger.info(mem.toString());
+        log.info(mem.toString());
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(mem.getUpw());
 
@@ -349,22 +455,28 @@ public class MemberController {
         rttr.addAttribute("name", mem.getName());
         rttr.addFlashAttribute("msg", "SUCCESS");
 
-        logger.info(rttr.toString());
+        log.info(rttr.toString());
 
         return "SUCCESS";
 
     }
 
-    // 회원탈퇴
+    /**
+     * Delete string.
+     *
+     * @param mem the mem
+     * @return the string
+     * @throws Exception the exception
+     */
+// 회원탈퇴
     @RequestMapping(value = "/remove", method = RequestMethod.POST)
     public String delete(@RequestBody MemberEntity mem) throws Exception {
 
-        logger.info("delete post ...........");
-        logger.info(mem.toString());
+        log.info("delete post ...........");
+        log.info(mem.toString());
 
-       mapper.delete(mem.getUuid());
+        mapper.delete(mem.getUuid());
 
         return "succ";
     }
-
 }
