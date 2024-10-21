4e22ce72ef1f43fbaf4b63fe5a6561fea2523db1
diffth <diffth@nate.com>
2024-10-21 14:44:28 +0900

    1021 5st commit~ NewsController

--- src/main/java/com/healthplan/work/Controller/NewsController.java
+++ src/main/java/com/healthplan/work/Controller/NewsController.java
@@ -4,10 +4,7 @@ import com.healthplan.work.service.NewsService;
 import com.healthplan.work.vo.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +19,7 @@ public class NewsController {
 	private NewsService newsService;
 
 	@GetMapping("/news")
-	public Map<String, Object> news() throws Exception {
+	public @ResponseBody Map<String, Object> news() throws Exception {
 		Map<String, Object> result = new HashMap<>();
 
 		List<NewsEntity> newslist = newsService.selectList();
